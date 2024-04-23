841ca2d0a18d87f04a8ed464764e121191a72baf
ctsirka <xarat19@gmail.com>
2024-04-23 21:07:14 +0300

    Add user info screen

--- ui/front.py
+++ ui/front.py
@@ -7,7 +7,9 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 from kivy.core.window import Window
 from kivymd.app import MDApp
-
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.pickers import MDDockedDatePicker
+from kivy.metrics import dp
 
 
 
@@ -26,7 +28,82 @@ class LoginScreen(Screen):
         self.add_widget(fl)
 
 class MainScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+                
+        
+        fl = FloatLayout()
+        bg = Image(source='main_background.png', allow_stretch=True, keep_ratio=False)
+        fl.add_widget(bg)
+        self.add_widget(fl)
+
+        self.menu = None
+    
+    def open_area_menu(self):
+        if self.menu:
+            self.menu.open()
+
+    def on_enter(self):
+        # Assuming the TextField id is 'area_field' in your .kv file
+        area_field = self.ids.area_field
+        
+        menu_items = [
+            {"text": "Rural", "on_release": lambda x="Rural": self.set_item(x)},
+            {"text": "Urban", "on_release": lambda x="Urban": self.set_item(x)}
+        ]
+        
+        self.menu = MDDropdownMenu(
+            caller=area_field,  # Caller is now the TextField
+            items=menu_items,
+            width_mult=4,
+            position="bottom"
+        )
+
+    def set_item(self, text):
+        # This method sets the selected item's text to the TextField
+        self.ids.area_field.text = text
+        self.menu.dismiss()
+    
+    def on_cancel(self, *args):
+        if self.date_dialog:
+            self.date_dialog.dismiss()
+
+    def on_ok(self, *args):
+        date_list = self.date_dialog.get_date()
+        if date_list:  # Check if the list is not empty
+            selected_date = date_list[0]  # Extract the first item (the date object)
+            formatted_date = selected_date.strftime('%m/%d/%Y')
+            print(formatted_date)
+        
+            self.ids.dob.text = formatted_date
+            self.ids.licence_issue_date_field.text = formatted_date
+
+            
+            
+            
+        print(self.date_dialog.get_date())
+        self.on_cancel()
+
+    def show_date_picker(self, widget, focus):
+        if not focus:
+            return
+        
+        
+        date_dialog = MDDockedDatePicker()
+        self.date_dialog = date_dialog
+        self.date_dialog.bind(
+            on_ok=self.on_ok,
+            on_cancel=self.on_cancel,
+        )
+        # Set the position based on the widget's properties
+        date_dialog.pos = [
+            widget.center_x - date_dialog.width / 2,
+            widget.y - (date_dialog.height - dp(142)),
+        ]
+
+
+        date_dialog.open()
+
 
 class ScreenManager(ScreenManager):
     pass
