3cf6fd7695232e84993fbdaf847933a294524783
ctsirka <xarat19@gmail.com>
2024-05-02 13:36:15 +0300

    all screens are functional

--- ui/front.py
+++ ui/front.py
@@ -11,23 +11,70 @@ from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.pickers import MDDockedDatePicker
 from kivy.metrics import dp
 
+class DatePickerMixin:
+    date_dialog = None
 
+    def show_date_picker(self, widget, focus):
+        if not focus:
+            return
+        
+        self.trigger_widget = widget
 
+        if not self.date_dialog:
+            self.date_dialog = MDDockedDatePicker()
+            self.date_dialog.bind(on_ok=self.on_ok, on_cancel=self.on_cancel)
+        
+        # Positioning the dialog based on the widget's properties
+        self.date_dialog.pos = [
+            widget.center_x - self.date_dialog.width / 2,
+            widget.y - (self.date_dialog.height - dp(142))
+        ]
+        self.date_dialog.open()
+
+    def on_ok(self, instance):
+        value = instance.get_date()[0]
+        if value:
+            formatted_date = value.strftime('%m/%d/%Y')
+            if hasattr(self, 'trigger_widget') and self.trigger_widget:
+                self.trigger_widget.text = formatted_date
+            self.date_dialog.dismiss()
+
+    def on_cancel(self,instance):
+        if self.date_dialog:
+            self.date_dialog.dismiss()
+
+class DropdownMenuMixin:
+    def create_menu(self, textfield, menu_items):
+        """Create and return a dropdown menu for the specified text field and menu items."""
+        menu = MDDropdownMenu(
+            caller=textfield,
+            items=menu_items,
+            width_mult=4,
+            position='bottom'
+        )
+        return menu
+
+    def open_menu(self, menu):
+        """Open the specified menu."""
+        if menu:
+            menu.open()
+
+    def set_item(self, text, menu, text_field):
+        """Set the selected item's text to the TextField and dismiss the menu."""
+        text_field.text = text
+        menu.dismiss()
 
 #Define different screens
 class LoginScreen(Screen):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-        #Window.size = (800, 600)  # Width = 800, Height = 600
-        #Window.resizable = False
-        
         
         fl = FloatLayout()
         bg = Image(source='login_background.png', allow_stretch=True, keep_ratio=False)
         fl.add_widget(bg)
         self.add_widget(fl)
 
-class MainScreen(Screen):
+class MainScreen(Screen, DatePickerMixin, DropdownMenuMixin):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
                 
@@ -38,73 +85,31 @@ class MainScreen(Screen):
         self.add_widget(fl)
 
         self.menu = None
+   
     
-    def open_area_menu(self):
+    def on_leave(self):
+        # Make sure to clean up by dismissing the menu if it's open
         if self.menu:
-            self.menu.open()
-
-    def on_enter(self):
-        # Assuming the TextField id is 'area_field' in your .kv file
-        area_field = self.ids.area_field
-        
-        menu_items = [
-            {"text": "Rural", "on_release": lambda x="Rural": self.set_item(x)},
-            {"text": "Urban", "on_release": lambda x="Urban": self.set_item(x)}
-        ]
-        
-        self.menu = MDDropdownMenu(
-            caller=area_field,  # Caller is now the TextField
-            items=menu_items,
-            width_mult=4,
-            position="bottom"
-        )
-
-    def set_item(self, text):
-        # This method sets the selected item's text to the TextField
-        self.ids.area_field.text = text
-        self.menu.dismiss()
+            self.menu.dismiss()
     
-    def on_cancel(self, *args):
-        if self.date_dialog:
-            self.date_dialog.dismiss()
-
-    def on_ok(self, *args):
-        date_list = self.date_dialog.get_date()
-        if date_list:  # Check if the list is not empty
-            selected_date = date_list[0]  # Extract the first item (the date object)
-            formatted_date = selected_date.strftime('%m/%d/%Y')
-            print(formatted_date)
-        
-            self.ids.dob.text = formatted_date
-            self.ids.licence_issue_date_field.text = formatted_date
-
+    def on_dropdown_focus(self, instance, value):
+        area_field = self.ids.area_field
             
-            
-            
-        print(self.date_dialog.get_date())
-        self.on_cancel()
-
-    def show_date_picker(self, widget, focus):
-        if not focus:
-            return
-        
-        
-        date_dialog = MDDockedDatePicker()
-        self.date_dialog = date_dialog
-        self.date_dialog.bind(
-            on_ok=self.on_ok,
-            on_cancel=self.on_cancel,
-        )
-        # Set the position based on the widget's properties
-        date_dialog.pos = [
-            widget.center_x - date_dialog.width / 2,
-            widget.y - (date_dialog.height - dp(142)),
+        menu_items = [
+            {"text": "Rural", "on_release": lambda x="Rural": self.set_item(x, self.menu, area_field)},
+            {"text": "Urban", "on_release": lambda x="Urban": self.set_item(x, self.menu, area_field)}
         ]
+        
+        self.menu = self.create_menu(area_field, menu_items)
+        self.open_menu(self.menu) 
+            
 
+    def on_text_field_focus(self, instance, value):
+        if value:  # if the text field is focused
+            self.show_date_picker(instance,value)
+    
 
-        date_dialog.open()
-
-class VehicleScreen(Screen):
+class VehicleScreen(Screen, DropdownMenuMixin):
     def __init__(self, **kwargs):
         super(VehicleScreen, self).__init__(**kwargs)
                 
@@ -113,35 +118,25 @@ class VehicleScreen(Screen):
         bg = Image(source='main_background.png', allow_stretch=True, keep_ratio=False)
         fl.add_widget(bg)
         self.add_widget(fl)
+
+    def on_dropdown_focus(self, instance, value):
+        if instance == self.ids.type_field:
+            menu_items = [
+            {"text": "Passenger Car", "on_release": lambda x="Passenger Car": self.set_item(x,self.menu, instance)},
+            {"text": "Motorbike", "on_release": lambda x="Motorbike": self.set_item(x,self.menu, instance)},
+            {"text": "Van", "on_release": lambda x="Van": self.set_item(x,self.menu, instance)},
+            {"text": "Agricultural Vehicle", "on_release": lambda x="Agricultural Vehicle": self.set_item(x,self.menu, instance)}
+            ]
+        elif instance == self.ids.fuel_field:   
+            menu_items = [
+            {"text": "Petrol", "on_release": lambda x="Petrol": self.set_item(x,self.menu, instance)},
+            {"text": "Diesel", "on_release": lambda x="Diesel": self.set_item(x,self.menu, instance)}
+            ]
+        self.menu = self.create_menu(instance, menu_items)
+        self.open_menu(self.menu) 
     
-    def open_type_menu(self):
-        if self.menu:
-            self.menu.open()
 
-    def on_enter(self):
-        # Assuming the TextField id is 'area_field' in your .kv file
-        type_field = self.ids.type_field
-        
-        menu_items = [
-            {"text": "Passenger Car", "on_release": lambda x="Passenger Car": self.set_item(x)},
-            {"text": "Motorbike", "on_release": lambda x="Motorbike": self.set_item(x)},
-            {"text": "Van", "on_release": lambda x="Van": self.set_item(x)},
-            {"text": "Agricultural Vehicle", "on_release": lambda x="Agricultural Vehicle": self.set_item(x)}
-        ]
-        
-        self.menu = MDDropdownMenu(
-            caller=type_field,  # Caller is now the TextField
-            items=menu_items,
-            width_mult=4,
-            position="bottom"
-        )
-
-    def set_item(self, text):
-        # This method sets the selected item's text to the TextField
-        self.ids.type_field.text = text
-        self.menu.dismiss()
-
-class InsuranceScreen(Screen):
+class InsuranceScreen(Screen, DatePickerMixin, DropdownMenuMixin):
     def __init__(self, **kwargs):
         super(InsuranceScreen, self).__init__(**kwargs)
                 
@@ -151,44 +146,29 @@ class InsuranceScreen(Screen):
         fl.add_widget(bg)
         self.add_widget(fl)
 
+    def on_text_field_focus(self, instance, value):
+        if value:  # if the text field is focused
+            self.show_date_picker(instance,value)
+    
+    def on_dropdown_focus(self, instance, value):
+        if instance == self.ids.driver:
+            menu_items = [
+            {"text": "Yes", "on_release": lambda x="Yes": self.set_item(x,self.menu, instance)},
+            {"text": "No", "on_release": lambda x="No": self.set_item(x,self.menu, instance)}
+            ]
+        elif instance == self.ids.channel:   
+            menu_items = [
+            {"text": "Agent", "on_release": lambda x="Agent": self.set_item(x,self.menu, instance)},
+            {"text": "Insurance Brokers", "on_release": lambda x="Insurance Brokers": self.set_item(x,self.menu, instance)}
+            ]
+        elif instance == self.ids.payment:
+            menu_items = [
+            {"text": "Half-yearly", "on_release": lambda x="Half-yearly": self.set_item(x,self.menu, instance)},
+            {"text": "Annual", "on_release": lambda x="Annual": self.set_item(x,self.menu, instance)}
+            ]
+        self.menu = self.create_menu(instance, menu_items)
+        self.open_menu(self.menu) 
 
-    def on_cancel(self, *args):
-        if self.date_dialog:
-            self.date_dialog.dismiss()
-
-    def on_ok(self, *args):
-        date_list = self.date_dialog.get_date()
-        if date_list:  # Check if the list is not empty
-            selected_date = date_list[0]  # Extract the first item (the date object)
-            formatted_date = selected_date.strftime('%m/%d/%Y')
-            print(formatted_date)
-        
-            self.ids.dob.text = formatted_date
-            self.ids.licence_issue_date_field.text = formatted_date
-
-            
-            
-            
-        print(self.date_dialog.get_date())
-        self.on_cancel()
-
-    def show_date_picker(self, widget, focus):
-        if not focus:
-            return
-        
-        
-        date_dialog = MDDockedDatePicker()
-        self.date_dialog = date_dialog
-        self.date_dialog.bind(
-            on_ok=self.on_ok,
-            on_cancel=self.on_cancel,
-        )
-        # Set the position based on the widget's properties
-        date_dialog.pos = [
-            widget.center_x - date_dialog.width / 2,
-            widget.y - (date_dialog.height - dp(142)),
-        ]
-        date_dialog.open()
 
 
 class PriceScreen(Screen):
